64f3638c1c7d87480b3a41e22f68319e6092ead7
nehal <kadyannehal333@gmail.com>
2025-08-16 10:18:04 +0530

    lecture_2 - swiggy UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,62 +1,202 @@
-import { Button, Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity } from "react-native";
-import reactLogo from "../assets/images/react-logo.png";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function Index() {
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.heading}>This is my App!</Text>
+    // <SafeAreaView style={styles.container}>
+    //   <Text style={styles.heading}>This is my App!</Text>
 
-      {/* Image */}
+    //   {/* Image */}
 
-      <Image
-        style={{ height: 200, width: 400 }}
-        source={{
-          uri: "https://images.unsplash.com/photo-1754597302822-4b96f3442d3f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        }}
-      ></Image>
+    //   <Image
+    //     style={{ height: 200, width: 400 }}
+    //     source={{
+    //       uri: "https://images.unsplash.com/photo-1754597302822-4b96f3442d3f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    //     }}
+    //   ></Image>
 
-      <Image source={reactLogo}></Image>
+    //   <Image source={reactLogo}></Image>
 
-      {/* Text Input */}
+    //   {/* Text Input */}
 
+    //   <TextInput
+    //     placeholderTextColor="gray"
+    //     placeholder="Enter here..."
+    //   ></TextInput>
+
+    //   {/* Buttons */}
+
+    //   <Button title = "Submit"></Button>
+
+    //   {/* TouchableOpacity */}
+
+    //   <TouchableOpacity>
+    //     <Text>Register</Text>
+    //   </TouchableOpacity>
+
+    //   {/* Touchable Highlight */}
+
+    //   <TouchableHighlight>
+    //     <Text>Highlighted button</Text>
+    //   </TouchableHighlight>
+
+    // </SafeAreaView>
+
+    <SafeAreaView style={{ padding: 10 }}>
       <TextInput
         placeholderTextColor="gray"
-        placeholder="Enter here..."
+        placeholder="Serach for restaurant, item or more..."
+        style={styles.searchBox}
       ></TextInput>
 
-      {/* Buttons */}
+      <View style={styles.imgContainer}>
+        <Image
+          style={{ height: 200, width: "100%", borderRadius: 10 }}
+          source={{
+            uri: "https://images.unsplash.com/photo-1635526910370-6881e1756fee?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          }}
+        ></Image>
+      </View>
 
-      <Button title = "Submit"></Button>
+      <View style={{ padding: 10 }}>
+        <Text style={styles.subtitle}>What's on your mind?</Text>
 
-      {/* TouchableOpacity */}
+        <ScrollView horizontal style = {{paddingTop: 10, marginLeft: -10}}>
+          <View style={styles.itemContainer}>
+            <View>
+              <Image
+                style={styles.itemImage}
+                source={{
+                  uri: "https://images.unsplash.com/photo-1563379091339-03b21ab4a4f8?q=80&w=810&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                }}
+              ></Image>
+            </View>
 
-      <TouchableOpacity>
-        <Text>Register</Text>
-      </TouchableOpacity>
+            <View>
+              <Text style = {styles.itemName}>Biryani</Text>
+            </View>
+          </View>
 
+          <View style={styles.itemContainer}>
+            <View>
+              <Image
+                style={styles.itemImage}
+                source={{
+                  uri: "https://images.unsplash.com/photo-1551024601-bec78aea704b?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                }}
+              ></Image>
+            </View>
 
-      {/* Touchable Highlight */}
+            <View>
+              <Text  style = {styles.itemName}>Dessert</Text>
+            </View>
+          </View>
 
-      <TouchableHighlight>
-        <Text>Highlighted button</Text>
-      </TouchableHighlight>
+          <View style={styles.itemContainer}>
+            <View>
+              <Image
+                style={styles.itemImage}
+                source={{
+                  uri: "https://images.unsplash.com/photo-1551024601-bec78aea704b?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                }}
+              ></Image>
+            </View>
 
+            <View>
+              <Text  style = {styles.itemName}>Dessert</Text>
+            </View>
+          </View>
 
+          <View style={styles.itemContainer}>
+            <View>
+              <Image
+                style={styles.itemImage}
+                source={{
+                  uri: "https://images.unsplash.com/photo-1551024601-bec78aea704b?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                }}
+              ></Image>
+            </View>
+
+            <View>
+              <Text  style = {styles.itemName}>Dessert</Text>
+            </View>
+          </View>
+
+          <View style={styles.itemContainer}>
+            <View>
+              <Image
+                style={styles.itemImage}
+                source={{
+                  uri: "https://images.unsplash.com/photo-1551024601-bec78aea704b?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                }}
+              ></Image>
+            </View>
+
+            <View>
+              <Text  style = {styles.itemName}>Dessert</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
 
 // stylesheet
 const styles = StyleSheet.create({
-  heading: {
-    fontSize: 20,
-    color: "purple",
+  // heading: {
+  //   fontSize: 20,
+  //   color: "purple",
+  // },
+
+  // container: {
+  //   flex: 1,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  // },
+
+  searchBox: {
+    padding: 15,
+    borderColor: "black",
+    borderWidth: 1,
+    backgroundColor: "white",
+    borderRadius: 10,
+    fontSize: 17,
   },
 
-  container: {
+  imgContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 10,
   },
+
+  subtitle: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+
+  itemContainer: {
+    flex: 1,
+    width: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  itemImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+
+  itemName : {
+    fontSize : 14
+  }
 });
