570e5f6e7c436279058077f8e203c50adee5ba20
Nehal Kadyan <kadyannehal333@gmail.com>
2025-09-10 12:33:38 +0530

    displaying users in flatlist

--- app/(app)/(tabs)/services.tsx
+++ app/(app)/(tabs)/services.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, FlatList } from "react-native";
 
 const services = () => {
   // state for users
   const [users, setUsers] = useState([]);
 
   console.log("users", users);
-
   // function to fetch users
   const fetchUsers = async () => {
     // logic
@@ -18,7 +17,6 @@ const services = () => {
       // converting response to json
 
       const data = await response.json();
-
       setUsers(data);
     } catch (err) {
       console.log(err);
@@ -30,8 +28,63 @@ const services = () => {
   }, []);
 
   return (
-    <View>
-      <Text>Services Tab</Text>
+    <View style={{ flex: 1, backgroundColor: "white", padding: 20 }}>
+      {/* <Text>Services Tab</Text> */}
+
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+          textAlign: "center",
+          padding: 20,
+        }}
+      >
+        Users List:
+      </Text>
+
+      <Text
+        style={{
+          textAlign: "center",
+          fontWeight: "500",
+          fontSize: 17,
+          padding: 10,
+        }}
+      >
+        Total Users : {users.length}
+      </Text>
+
+      {/* Displaying List of users */}
+
+      <FlatList
+        data={users}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              padding: 10,
+              gap: 10,
+              backgroundColor: "#E3DEDC",
+              borderWidth: 1,
+              borderColor: "black",
+              borderRadius: 15,
+              marginBottom: 16,
+            }}
+          >
+            <Text style={{ fontSize: 19, fontWeight: "600" }}>
+              Name : {item.name}
+            </Text>
+            <Text style={{ fontSize: 17, fontWeight: "400" }}>
+              Email : {item.email}
+            </Text>
+            <Text style={{ fontSize: 17, fontWeight: "400" }}>
+              Company : {item.company.name}
+            </Text>
+            <Text style={{ fontSize: 17, fontWeight: "400" }}>
+              Website : {item.website}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
